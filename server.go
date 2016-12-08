cc3159ba9bac6aff56981d03e857fd83a75016e5
Tom Johnsen <t.johnsen8@gmail.com>
2016-12-07 21:57:30 -0500

    initial commit with html template and mongo example

--- server.go
+++ server.go
@@ -2,14 +2,75 @@ package main
 
 import (
 	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 )
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+type Person struct {
+	Name  string
+	Phone string
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) *Page {
+	filename := title + ".txt"
+	body, _ := ioutil.ReadFile(filename)
+	return &Page{Title: title, Body: body}
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "nah %s", r.URL.Path[1:])
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p := loadPage(title)
+	renderTemplate(w, "view", p)
+}
+
 func main() {
+	session, err := mgo.Dial("127.0.0.1:27017")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+
+	c := session.DB("test").C("people")
+	err = c.Insert(&Person{"Ale", "+55 53 8116 9639"},
+		&Person{"Cla", "+55 53 8402 8510"})
+	if err != nil {
+		panic(err)
+	}
+
+	result := Person{}
+	err = c.Find(bson.M{"name": "Ale"}).One(&result)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Phone:", result.Phone)
+
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
 	http.ListenAndServe(":8080", nil)
 }
